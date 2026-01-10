d19a53d7365ccb74ec49cb9fa025de42e0c7dfdd
J-Bean-Desktop-Mint <cryosonic@hotmail.co.uk>
2026-01-10 18:33:03 +0000

    Submitting answers for foundational Javascript exercise by The Odin Project

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let converted = 0;
+  converted = (Number(fahren) - 32) * (5/9);
+  if (converted - Math.floor(converted) == 0) {
+    return converted;
+  } else {
+    return Number(converted.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let converted = 0;
+  converted = (Number(cels) * (9/5)) + 32;
+  if (converted - Math.floor(converted) == 0) {
+    return converted;
+  } else {
+    return Number(converted.toFixed(1));
+  }
 };
 
 // Do not edit below this line
