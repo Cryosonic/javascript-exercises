7197e45be6f0f29b83c86b288462b9578c0c044d
J-Bean-Desktop-Mint <cryosonic@hotmail.co.uk>
2026-01-10 15:41:15 +0000

    Submission for exercise 09 from foundation Javascript by The Odin Project

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,42 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+  let result = 0;
 
+  const sumRange = (small, large) => {
+    for (let i = small; i <= large; i++) {
+      result += i;
+    }
+  }
+
+  const checkNegative = (num) => {return num <= 0 ? false : true;}
+
+  const checkFloat = (num) => {
+    if (checkNegative(num)) {
+      if (num - Math.floor(num) != 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+    
+  }
+
+  const checkNumber = (num) => {
+    if (checkFloat(num)) {
+      return typeof num != "number" ? false : true;
+    }
+  }
+
+  if (checkNumber(firstNum) && checkNumber(secondNum)) {
+    if (firstNum < secondNum) {
+      sumRange(firstNum, secondNum);
+    } else {
+      sumRange(secondNum, firstNum);
+    }
+  } else {
+    result = "ERROR";
+  }
+
+  return result;
 };
 
 // Do not edit below this line
