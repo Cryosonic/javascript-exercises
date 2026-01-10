488b74c22749964999abb8d4d46dac22392c7de8
J-Bean-Desktop-Mint <cryosonic@hotmail.co.uk>
2026-01-10 12:51:54 +0000

    submitting answers to foundation javascript exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let result = "";
+  let strInArray = str.split("");
+  for(let i = str.length-1; i >= 0; i--) {
+    result += strInArray[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
