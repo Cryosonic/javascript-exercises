a8ff0102adbf260bba81cac701712b66c7e5c56d
J-Bean-Desktop-Mint <cryosonic@hotmail.co.uk>
2026-01-10 12:50:54 +0000

    completed solution to foundation javascript exercise 07

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+  const resultArray = arr;
+  const toRemove = targets;
+
+  toRemove.forEach((el)=>{
+    if(resultArray.includes(el)) {
+      while(resultArray.includes(el)) {
+        resultArray.splice(resultArray.indexOf(el),1)
+      }
+    }
+  })
+
+  return resultArray;
 };
 
 // Do not edit below this line
